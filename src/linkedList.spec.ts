2d37137356b3937f992db9151b846d67993699ba
Alice Spagnesi <alicespagnesi@air-de-alice.home>
2025-01-06 21:17:35 +0100

    feat: add size, hasCycle and reverse

--- src/linkedList.spec.ts
+++ src/linkedList.spec.ts
@@ -5,6 +5,7 @@ describe('LinkedList class', () => {
     const list = new LinkedList();
     expect(list.head).toEqual(null);
     expect(list.tail).toEqual(null);
+    expect(list.size).toEqual(0);
   })
 
   it('list with one node', () => {
@@ -13,6 +14,7 @@ describe('LinkedList class', () => {
     list.prepend(node1);
     expect(list.head?.value).toEqual(4);
     expect(list.tail?.value).toEqual(4);
+    expect(list.size).toEqual(1);
   })
 
   it('list with two nodes', () => {
@@ -25,6 +27,7 @@ describe('LinkedList class', () => {
     expect(list.head?.value).toEqual(5);
     expect(list.head?.next?.value).toEqual(4);
     expect(list.tail?.value).toEqual(4);
+    expect(list.size).toEqual(2);
   })
 
   it('list with two nodes', () => {
@@ -42,6 +45,58 @@ describe('LinkedList class', () => {
     expect(list.head?.value).toEqual(5);
     expect(list.head?.next).toEqual(null);
 
+  })
+
+  it('list with no cycle', () => {
+    const list = new LinkedList();
+    const node1 = new LinkedNode(1, null);
+    const node2 = new LinkedNode(2, null);
+    const node3 = new LinkedNode(3, null);
+    const node4 = new LinkedNode(4, null);
+
+    list.append(node1);
+    list.append(node2);
+    list.append(node3);
+    list.append(node4);
+
+    expect(list.hasCycle()).toEqual(false);
+
+  })
+
+  it('list with cycle', () => {
+    const list = new LinkedList();
+    const node1 = new LinkedNode(1, null);
+    const node2 = new LinkedNode(2, null);
+    const node3 = new LinkedNode(3, null);
+    const node4 = new LinkedNode(4, null);
+    const node5 = new LinkedNode(5, null);
+    const node6 = new LinkedNode(6, null);
+
+    list.append(node1);
+    list.append(node2);
+    list.append(node3);
+    list.append(node4);
+    list.append(node3);
+
+    expect(list.hasCycle()).toEqual(true);
+
+  })
+
+  it('list reverse', () => {
+    const list = new LinkedList();
+    const node1 = new LinkedNode(1, null);
+    const node2 = new LinkedNode(2, null);
+    const node3 = new LinkedNode(3, null);
+    const node4 = new LinkedNode(4, null);
+
+    list.append(node1);
+    list.append(node2);
+    list.append(node3);
+    list.append(node4);
+    list.reverse();
+
+    expect(list.head?.value).toEqual(4);
+    expect(list.tail?.value).toEqual(1);
   })
 
 })
--- src/linkedList.ts
+++ src/linkedList.ts
@@ -11,52 +11,88 @@ export class LinkedNode<T> {
 export class LinkedList<T>{
     head: LinkedNode<T> | null = null;
     tail: LinkedNode<T> | null = null;
+    size: number = 0;
+
 
     prepend(node: LinkedNode<T>): void {
-
-        this.head = new LinkedNode(node.value, this.head);
+        node.next = this.head;
+        this.head = node;
         if (!this.tail) {
-            this.tail = this.head;
+            this.tail = node;
         }
-
+        this.size++;
     }
 
     append(node: LinkedNode<T>): void {
         if (!this.head) {
-            this.head = this.tail;
+            this.head = node;
+            this.tail = node;
+        } else {
+            if (this.tail) this.tail.next = node;
+            this.tail = node;
         }
-
-        let oldTail = this.tail;
-        if (oldTail) oldTail.next = node;
-        this.tail = node;
+        this.size++;
     }
 
     delete(nodeToDelete: LinkedNode<T>): void {
-        if(!this.head) {
+        if (!this.head) {
             return;
         }
 
+        // If head is the node to delete
         if (this.head === nodeToDelete) {
-
-            if (!this.head.next) {
-                this.head =  null;
-                this.tail = null;
-            } else {
-                this.head = this.head.next;
-                return;
+            this.head = this.head.next;
+            if (this.head === null) {
+                this.tail = null;  // list becomes empty
             }
+            this.size--;
+            return;
         }
 
         let node = this.head;
-        while(node?.next) {
-            if (node.next = nodeToDelete) {
+        while (node.next) {
+            if (node.next === nodeToDelete) {
                 node.next = node.next.next;
+                if (node.next === null) {
+                    this.tail = node;  // Update tail if the last node is deleted
+                }
+                this.size--;
                 return;
             }
             node = node.next;
         }
-
-
     }
 
+    hasCycle(): boolean {
+        if (!this.head || !this.tail) return false;
+
+        let slow: LinkedNode<T> | null = this.head;
+        let fast: LinkedNode<T> | null = this.head;
+
+        while(fast != null && fast.next != null && slow != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+    reverse(): void{
+        let current = this.head;
+        let prev: LinkedNode<T> | null = null;
+        let next: LinkedNode<T> | null = null;
+        this.tail = this.head;  // After reversal, head becomes tail
+
+        while (current) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        this.head = prev;
+
+    }
 }
